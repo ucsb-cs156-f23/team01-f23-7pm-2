1de81defa4d716cec05305b16e8fb4aac4deea60
tigeryu8900 <tigeryu8900@gmail.com>
2023-10-17 16:18:01 -0700

    ty - use log.info for joke query service

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/JokeQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/JokeQueryService.java
@@ -1,5 +1,6 @@
 package edu.ucsb.cs156.spring.backenddemo.services;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ import java.util.Map;
 
 
 @Service
+@Slf4j
 public class JokeQueryService {
 
     private final RestTemplate restTemplate;
@@ -22,6 +24,7 @@ public class JokeQueryService {
     public static final String ENDPOINT = "https://v2.jokeapi.dev/joke/{category}?amount={numJokes}";
 
     public String getJSON(String category, int numJokes) throws HttpClientErrorException {
+        log.info("category={}&amount={}", category, numJokes);
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(List.of(MediaType.APPLICATION_JSON));
         headers.setContentType(MediaType.APPLICATION_JSON);
