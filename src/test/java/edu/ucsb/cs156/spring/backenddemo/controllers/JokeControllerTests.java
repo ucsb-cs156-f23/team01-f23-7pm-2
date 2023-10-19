bacae6b31006b22782f22af6032d27a5c790c7f9
tigeryu8900 <tigeryu8900@gmail.com>
2023-10-19 14:30:32 -0700

    ty - use "{ \"fake\" : \"result\" }" as fakeJsonResult

--- src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/JokeControllerTests.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/JokeControllerTests.java
@@ -1,6 +1,5 @@
 package edu.ucsb.cs156.spring.backenddemo.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.ucsb.cs156.spring.backenddemo.services.JokeQueryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +17,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @WebMvcTest(value = JokeController.class)
 public class JokeControllerTests {
-    private ObjectMapper mapper = new ObjectMapper();
     @Autowired
     private MockMvc mockMvc;
     @MockBean
@@ -27,46 +25,7 @@ public class JokeControllerTests {
 
     @Test
     public void test_getJokes() throws Exception {
-        String fakeJsonResult="""
-                {
-                    "error": false,
-                    "amount": 2,
-                    "jokes": [
-                        {
-                            "category": "Programming",
-                            "type": "single",
-                            "joke": "A SQL statement walks into a bar and sees two tables.\\nIt approaches, and asks \\"may I join you?\\"",
-                            "flags": {
-                                "nsfw": false,
-                                "religious": false,
-                                "political": false,
-                                "racist": false,
-                                "sexist": false,
-                                "explicit": false
-                            },
-                            "id": 5,
-                            "safe": true,
-                            "lang": "en"
-                        },
-                        {
-                            "category": "Programming",
-                            "type": "twopart",
-                            "setup": "How do you know God is a shitty programmer?",
-                            "delivery": "He wrote the OS for an entire universe, but didn't leave a single useful comment.",
-                            "flags": {
-                                "nsfw": false,
-                                "religious": true,
-                                "political": false,
-                                "racist": false,
-                                "sexist": false,
-                                "explicit": true
-                            },
-                            "id": 19,
-                            "safe": false,
-                            "lang": "en"
-                        }
-                    ]
-                }""";
+        String fakeJsonResult="{ \"fake\" : \"result\" }";
         String category = "Programming";
         int numJokes = 2;
         when(mockJokeQueryService.getJSON(eq(category), eq(numJokes))).thenReturn(fakeJsonResult);
